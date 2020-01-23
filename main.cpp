6d48b9a03889d51d78fba4badb2964a863ea3c03
Mark <markelliottfontenot@gmail.com>
2020-01-23 04:45:31 -0600

    Implemented simple new and delete

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
 
+using std::cout;
+using std::cin;
+using std::endl;
+
+class Point {
+private:
+    unsigned int x, y, z;
+public:
+    Point (unsigned int xval = 0, unsigned int yval = 0, unsigned int zval = 0):
+        x(xval), y(yval), z(zval) {
+        cout << "Initializing Point Object with [" << x << ", "
+                  << y << ", " << z << "]." << endl;
+    }
+
+    ~Point () {
+        cout << "Executing Point destructor" << endl;
+    }
+
+    void* operator new(size_t numBytes) {
+        cout << "Allocating Point with new: " << numBytes << " bytes." << endl;
+        return malloc(numBytes);
+    }
+
+    void operator delete(void* ptr) {
+        cout << "De-allocating Point with delete." << endl;
+        return free(ptr);
+    }
+
+
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Global new." << endl;
+    int* ptr01 = new int;
+    delete ptr01;
+    cout << "Specialized new." << endl;
+    Point* p = new Point();
+    delete p;
     return 0;
 }
