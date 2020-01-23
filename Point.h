c62dadc086d3f6e36a6ffc2c1fc998d1bb877b17
Mark <markelliottfontenot@gmail.com>
2020-01-23 07:13:56 -0600

    moved point class to separate file

---
+++ Point.h
@@ -0,0 +1,52 @@
+//
+// Created by mark on 1/23/20.
+//
+
+#include <cstddef>
+#include <iostream>
+#include "algoNew.h"
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+#ifndef MEMORYMANAGER_POINT_H
+#define MEMORYMANAGER_POINT_H
+class Point {
+private:
+    unsigned int x, y, z;
+public:
+    Point (unsigned int xval = 0, unsigned int yval = 0, unsigned int zval = 0):
+            x(xval), y(yval), z(zval) {
+        cout << "Initializing Point Object with [" << x << ", "
+             << y << ", " << z << "]." << endl;
+    }
+
+    ~Point () {
+        cout << "Executing Point destructor" << endl;
+    }
+
+    void* operator new(size_t numBytes) {
+        cout << "Allocating Point with new: " << numBytes << " bytes." << endl;
+        //return malloc(numBytes);
+        return ::new Point;
+
+    }
+
+    void operator delete(void* ptr) {
+        cout << "De-allocating Point with delete." << endl;
+        free(ptr);
+    }
+
+    void* operator new[](size_t numBytes) {
+        cout << "Allocating Point Array with new[]: " << numBytes << " bytes." << endl;
+        return malloc(numBytes);
+    }
+
+    void operator delete[] (void* ptr) {
+        cout << "De-allocating Point array with delete." << endl;
+        free(ptr);
+    }
+
+};
+#endif //MEMORYMANAGER_POINT_H
--- main.cpp
+++ main.cpp
@@ -1,46 +1,10 @@
 #include <iostream>
 #include "algoNew.h"
+#include "Point.h"
 using std::cout;
 using std::cin;
 using std::endl;
 
-class Point {
-private:
-    unsigned int x, y, z;
-public:
-    Point (unsigned int xval = 0, unsigned int yval = 0, unsigned int zval = 0):
-        x(xval), y(yval), z(zval) {
-        cout << "Initializing Point Object with [" << x << ", "
-                  << y << ", " << z << "]." << endl;
-    }
-
-    ~Point () {
-        cout << "Executing Point destructor" << endl;
-    }
-
-    void* operator new(size_t numBytes) {
-        cout << "Allocating Point with new: " << numBytes << " bytes." << endl;
-        //return malloc(numBytes);
-        return ::new Point;
-
-    }
-
-    void operator delete(void* ptr) {
-        cout << "De-allocating Point with delete." << endl;
-        free(ptr);
-    }
-
-    void* operator new[](size_t numBytes) {
-        cout << "Allocating Point Array with new[]: " << numBytes << " bytes." << endl;
-        return malloc(numBytes);
-    }
-
-    void operator delete[] (void* ptr) {
-        cout << "De-allocating Point array with delete." << endl;
-        free(ptr);
-    }
-
-};
 
 int main() {
     cout << "Global new." << endl;
